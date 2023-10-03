7d4629cda189502662a8edaf3ae126a353f5d32e
Dexh03 <pejfa24@gmail.com>
2023-10-03 15:43:29 -0700

    Add useEffect to manage player's turn and verify defeat

--- src/App.js
+++ src/App.js
@@ -1,52 +1,101 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2'
 
 function App() {
-  const [stackcards, setStackcards] = useState([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]);
+  const [stackcards, setStackcards] = useState([]);
   const [stack, setStack] = useState([]);
   const [upStackfirst, setupStackfirst] = useState([1]);
   const [upStackSecond, setupStackSecond] = useState([1]);
   const [downStackfirst, setdownStackfirst] = useState([100]);
   const [downStacksecond, setdownStacksecond] = useState([100]);
-  const [selectedCard, setselectedcard] =useState();
+  const [selectedCard, setselectedcard] = useState();
   const [allcards, setallcards] = useState([]);
   const [cardsThrown, setcardsThrown] = useState(0);
   const [startgame, setstartgame] = useState(false);
-  console.log(stack)
-
-console.log(cardsThrown)
-console.log(startgame)
+  const [turn, setTurn] = useState(1);
+  const[points,setPoints] = useState();
+  console.log(stackcards)
   useEffect(() => {
-    if (stack.length == 8){
-      setstartgame(true);
+    let array = []
+    for (let i = 2; i < 100; i++) {
+      array.push(i)
     }
-    console.log("Entro al componente")
+    setStackcards(array);
+  }, [])
+
+  useEffect(() => {
+    if (stack.length === 8) {
+      if (turn === 1) {
+        setstartgame(true);
+      }
+      setcardsThrown(0);
+      setTurn(turn + 1);
+    }
+    console.log("el turno es -->", turn)
+
+    setTimeout(() => {
+      validateDefeat();
+    }, 300)
   }, [stack]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (stackcards.length === 0 && stack.length === 0 && startgame) {
+        alert("Victoria");
+      }
+    }, 300)
+  }, [stack, stackcards])
+
+
   const takecard = () => {
-    let turno = 1; 
-  
     if (cardsThrown < 2 && startgame) {
       alert("Debes tirar al menos 2 cartas antes de tomar una nueva");
       return;
     } else if (stack.length < 8) {
       console.log("entro al obtener");
-      const randomIndex = Math.floor(Math.random() * stackcards.length);
-      const num = stackcards[randomIndex];
-      stackcards.splice(randomIndex, 1);
-      setStack([...stack, num]);
-      setallcards([...stack, num]);
-      if (startgame) {
-        setcardsThrown(cardsThrown + 1);
+      if (stackcards.length === 0) {
+        alert("El mazo de cartas por tirar está vacío. No puedes tomar más cartas.");
+      } else {
+        const randomNum = () => {
+          return Math.floor(Math.random() * stackcards.length);
+        }
+
+        const num = stackcards[randomNum()];
+        stackcards.splice(randomNum(), 1);
+        if (!startgame) {
+          let array = []
+          for (let i = 0; i < 8; i++) {
+            const randomNu = randomNum()
+            array.push(stackcards[randomNu])
+            setallcards([...stack, randomNu]);
+          }
+          console.log("array de juego es -->", array)
+          setStack(array);
+        } else {
+          console.log("el stack es -->",stack)
+          if(stack.length < 8){
+            const validateTotal = 8 - stack.length;
+            console.log("hacen falta ",validateTotal)
+            const numero = []
+            for (let i = 0; i < validateTotal; i++) {
+              const random = randomNum()
+              console.log("numero random es --->",random)
+              numero.push(random)
+            }
+            setStack([...stack, ...numero]);
+            setallcards([...stack, ...numero]);
+          }
+            
+        }
+        if (startgame) {
+          setcardsThrown(cardsThrown + 1);
+        }
       }
-    } else if (stack.length === 8) {
-      setcardsThrown(0);
-      alert("Fin del turno " + turno); 
-      turno++; 
     }
   };
 
-  const selectCard = (card) =>{
+  const selectCard = (card) => {
     setselectedcard(card)
     console.log(card)
   }
@@ -60,9 +109,24 @@ console.log(startgame)
         setupStackfirst(updatedStack);
         setStack(updatedHand);
         setselectedcard(null);
-        setcardsThrown(cardsThrown+1);
+        setcardsThrown(cardsThrown + 1);
       } else {
-        alert("La carta seleccionada debe ser exactamente 10 unidades menor que la carta superior del stack.");
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+
+        Toast.fire({
+          icon: 'warning',
+          title: 'Movimiento invalido, No puedes colocar un carta menor!'
+        })
       }
     }
   }
@@ -71,56 +135,101 @@ console.log(startgame)
   const insertCard1 = () => {
     if (selectedCard) {
       const topCard1 = upStackSecond[upStackSecond.length - 1];
-      if (!topCard1  || selectedCard=== topCard1 - 10||selectedCard > topCard1){
+      if (!topCard1 || selectedCard === topCard1 - 10 || selectedCard > topCard1) {
         const updatedStack1 = upStackSecond;
         const updatedHand1 = stack.filter((card) => card !== selectedCard);
         setupStackSecond([...updatedStack1, selectedCard]);
         setStack(updatedHand1);
         setselectedcard(null);
-        setcardsThrown(cardsThrown+1);
-      }else{
-        alert("La carta seleccionada debe ser exactamente 10 unidades menor que la carta superior del stack.");
+        setcardsThrown(cardsThrown + 1);
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+
+        Toast.fire({
+          icon: 'warning',
+          title: 'Movimiento invalido, No puedes colocar un carta menor!'
+        })
       }
-      
+
     }
   }
   const insertCard2 = () => {
     if (selectedCard) {
       const topCard2 = downStackfirst[downStackfirst.length - 1];
-      if (!topCard2  || selectedCard=== topCard2 + 10 ||selectedCard < topCard2){
-      const updatedStack = downStackfirst;
-      const updatedHand = stack.filter((card) => card !== selectedCard);
-      setdownStackfirst([...updatedStack, selectedCard]);
-      setStack(updatedHand);
-      setselectedcard(null);
-      setcardsThrown(cardsThrown+1);
-      }else{
-        alert("La carta seleccionada debe ser exactamente 10 unidades mayor que la carta superior del stack.");
+      if (!topCard2 || selectedCard === topCard2 + 10 || selectedCard < topCard2) {
+        const updatedStack = downStackfirst;
+        const updatedHand = stack.filter((card) => card !== selectedCard);
+        setdownStackfirst([...updatedStack, selectedCard]);
+        setStack(updatedHand);
+        setselectedcard(null);
+        setcardsThrown(cardsThrown + 1);
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+
+        Toast.fire({
+          icon: 'warning',
+          title: 'Movimiento invalido, No puedes colocar un carta mayor!'
+        })
       }
     }
   }
   const insertCard3 = () => {
     if (selectedCard) {
-      const topCard3 = downStacksecond[downStacksecond.length -1];
-      if (!topCard3 || selectedCard=== topCard3 + 10  ||selectedCard < topCard3){
+      const topCard3 = downStacksecond[downStacksecond.length - 1];
+      if (!topCard3 || selectedCard === topCard3 + 10 || selectedCard < topCard3) {
         const updatedStack = downStacksecond;
         const updatedHand = stack.filter((card) => card !== selectedCard);
         setdownStacksecond([...updatedStack, selectedCard]);
         setStack(updatedHand);
         setselectedcard(null);
-        setcardsThrown(cardsThrown+1);
-      }else{
-        alert("La carta seleccionada debe ser exactamente 10 unidades mayor que la carta superior del stack.")
+        setcardsThrown(cardsThrown + 1);
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+
+        Toast.fire({
+          icon: 'warning',
+          title: 'Movimiento invalido, No puedes colocar un carta mayor!'
+        })
       }
-      
+
     }
   }
   return (
     <>
       <div className="game-container">
         <div className="card-container">
-          <div className="up-stack" onClick={() => insertCard()} style={{userSelect:'none'}}> {upStackfirst[upStackfirst.length -1]}</div>
-          <div className="up-stack" onClick={() => insertCard1()} style={{userSelect:'none'}}>{upStackSecond[upStackSecond.length -1]}</div>
+          <div className="up-stack" onClick={() => insertCard()} style={{ userSelect: 'none' }}> {upStackfirst[upStackfirst.length - 1]}</div>
+          <div className="up-stack" onClick={() => insertCard1()} style={{ userSelect: 'none' }}>{upStackSecond[upStackSecond.length - 1]}</div>
         </div>
 
         <div className="card-container" onClick={() => takecard()}>
@@ -128,15 +237,15 @@ console.log(startgame)
         </div>
 
         <div className="card-container">
-          <div className="down-stack" onClick={()=> insertCard2()} style={{userSelect:'none'}}>{downStackfirst[downStackfirst.length -1]}</div>
-          <div className="down-stack" onClick={()=> insertCard3()} style={{userSelect:'none'}}>{downStacksecond[downStacksecond.length -1]}</div>
+          <div className="down-stack" onClick={() => insertCard2()} style={{ userSelect: 'none' }}>{downStackfirst[downStackfirst.length - 1]}</div>
+          <div className="down-stack" onClick={() => insertCard3()} style={{ userSelect: 'none' }}>{downStacksecond[downStacksecond.length - 1]}</div>
         </div>
       </div>
       <div className="player-hand">
         {stack.map((card, idx) => {
           return (
-            <div key={`card-${idx}`} className="player-hand1" style={{userSelect:'none'}}
-            onClick={()=>selectCard(card)}>
+            <div key={`card-${idx}`} className="player-hand1" style={{ userSelect: 'none' }}
+              onClick={() => selectCard(card)}>
               {card}
             </div>
           );
