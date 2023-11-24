64ce5914dee6c6f9fcfff00b7265e9cc9280abad
Dexh03 <pejfa24@gmail.com>
2023-11-24 15:27:28 -0700

    scoring path and added expert game

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import PaginaInicio from './inicio';
+import Puntaje from './puntaje';
+import Experto from './Experto';
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<PaginaInicio />} />
       <Route path="/juego/:seed" element={<App />} />
+      <Route path="/juego-experto/:seed" element={<Experto />} />
       <Route path="/juego/actual" element={<App />} />
+      <Route path="/puntaje" element={<Puntaje />} />
 
     </Routes>
   </Router>,
