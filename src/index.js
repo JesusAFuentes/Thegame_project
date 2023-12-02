bd852dd0a3d4f9e71d12cf6a5feaa5fdb0c7b150
Dexh03 <pejfa24@gmail.com>
2023-12-02 15:39:21 -0700

    changes from navbar

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import PaginaInicio from './inicio';
 import Puntaje from './puntaje';
 import Experto from './Experto';
+import Navbar from './inicio';
 
 ReactDOM.render(
   <Router>
     <Routes>
-      <Route path="/" element={<PaginaInicio />} />
+      <Route path="/" element={<Navbar />} />
       <Route path="/juego/:seed" element={<App />} />
       <Route path="/juego-experto/:seed" element={<Experto />} />
       <Route path="/juego/actual" element={<App />} />
